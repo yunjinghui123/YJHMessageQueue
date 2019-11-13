4f1715f52b19f31fbd28fe766eb7968b5668359b
yunjinghui <1432680302@qq.com>
2019-11-13 09:48:49 +0800

    加入opration自动释放池

--- YJHMessageQueue/YJHMessageOperation.m
+++ YJHMessageQueue/YJHMessageOperation.m
@@ -60,21 +60,23 @@
 }
 
 - (void)start {
-    if (self.isCancelled) {
-        [self finishedChange];
-        return;
-    }
-    [self executingChange];
-    __weak typeof(self) wSelf = self;
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self.animationView showOperationViewDuration:self.time isFinish:^(BOOL isFinish) {
-            __strong typeof(wSelf) strongSelf = wSelf;
-            if (strongSelf != self) {
-                return ;
-            }
+    @autoreleasepool {
+        if (self.isCancelled) {
             [self finishedChange];
-        }];
-    });
+            return;
+        }
+        [self executingChange];
+        __weak typeof(self) wSelf = self;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.animationView showOperationViewDuration:self.time isFinish:^(BOOL isFinish) {
+                __strong typeof(wSelf) strongSelf = wSelf;
+                if (strongSelf != self) {
+                    return ;
+                }
+                [self finishedChange];
+            }];
+        });
+    }
 }
 
 - (void)cancel {
